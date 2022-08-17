897ea31112e112f401ce9af45a28dfd24b71f5be
RAFAELREDONDO <ing.redondo.rafael@gmail.com>
2022-08-17 06:22:08 -0500

    iniciando repositorio

---
+++ PruebaGit2/HelloWordl.java
@@ -0,0 +1,13 @@
+public class HelloWordl {
+
+
+    public static void main(String[] args) {
+
+        String hola = "HEllo wordl git";
+
+        System.out.println("Escriba su nombre: ");
+        Scanner consola = new Scanner(System.in);
+        var usuario = consola.next();
+
+    }
+}
